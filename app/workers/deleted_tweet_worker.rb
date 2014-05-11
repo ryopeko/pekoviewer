8d5db50e990d3a0201f96664eb43ae3f8e513f10
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-05-12 00:22:44 +0900

    set retry

--- app/workers/deleted_tweet_worker.rb
+++ app/workers/deleted_tweet_worker.rb
@@ -1,5 +1,6 @@
 class DeletedTweetWorker
   include Sidekiq::Worker
+  sidekiq_options retry: 3
 
   def perform(tweet_id)
     tweet = Tweet.where(tweet_id: tweet_id).first or raise "Tweet (#{tweet_id}) not found"
