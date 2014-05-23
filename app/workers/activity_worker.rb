279064bcf7ae8696fe386e5b71035af03b85618d
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-05-24 00:42:13 +0900

    fix set wrong user_id

--- app/workers/activity_worker.rb
+++ app/workers/activity_worker.rb
@@ -2,9 +2,9 @@ class ActivityWorker
   include Sidekiq::Worker
 
   def perform(args)
-    user = ActorUser.where(actor_screen_name: args['actor_screen_name']).first
+    actor_user = ActorUser.where(actor_screen_name: args['actor_screen_name']).first
 
-    unless user
+    unless actor_user
       user = User.create(name: args['actor_screen_name'])
       ActorUser.create(
         user_id: user.id,
@@ -15,7 +15,7 @@ class ActivityWorker
 
     activity = Activity.new(
       actor_screen_name: args['actor_screen_name'],
-      user_id: user.id,
+      user_id: actor_user.user.id,
       source_service_name: args['source_service_name'],
       source_type: args['source_type'],
       description: args['description'],
